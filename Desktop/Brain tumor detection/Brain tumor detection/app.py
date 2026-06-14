b29db777cd0a5d5f7149f780a39fe0df4746a300
yash-kadole <yashkadole07@gmail.com>
2026-06-14 22:24:57 +0530

    Initial commit

---
+++ Desktop/Brain tumor detection/Brain tumor detection/app.py
@@ -0,0 +1,62 @@
+import os
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import cv2
+from keras.models import load_model
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__)
+
+
+model =load_model('BrainTumor10Epochs.h5')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def get_className(classNo):
+	if classNo==0:
+		return "No Brain Tumor"
+	elif classNo==1:
+		return "Yes Brain Tumor"
+
+
+def getResult(img_path, threshold=0.5):
+    image = cv2.imread(img_path)
+    image = Image.fromarray(image, 'RGB')
+    image = image.resize((64, 64))
+    image = np.array(image)
+    input_img = np.expand_dims(image, axis=0)
+    result = model.predict(input_img)
+    
+    print("Raw probabilities:", result)  # Add this line for debugging
+    
+    # Assuming a single output node (binary classification)
+    probability = result[0, 0]  # Access the probability for the first (and only) class
+    
+    return int(probability >= threshold)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        value=getResult(file_path)
+        result=get_className(value) 
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
